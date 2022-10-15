dbc2560337b1e93df1d9b475f0d696788862f996
jesse-ball <jesse.ball@bain.com>
2022-10-15 16:22:56 -0700

    init commit for repo

---
+++ src/SpacedRepition.py
@@ -0,0 +1,154 @@
+import argparse
+import csv
+import gzip
+import math
+import os
+import random
+import sys
+
+
+from collections import defaultdict, namedtuple
+
+MIN_HALF_LIFE = 15.0 / (24 * 60)    # 15 minutes
+MAX_HALF_LIFE = 274.                # 9 months
+LN2 = math.log(2.)
+
+class SpacedRepetitionModel(object):
+    """
+    Spaced repetition model. Implements the following approaches:
+      - 'hlr' (half-life regression; trainable)
+      - 'lr' (logistic regression; trainable)
+      - 'leitner' (fixed)
+      - 'pimsleur' (fixed)
+    """
+    def __init__(self, train, test, method='hlr', omit_h_term=False, initial_weights=None, lrate=.001, hlwt=.01, l2wt=.1, sigma=1.):
+        self.method = method
+        self.omit_h_term = omit_h_term
+        self.weights = defaultdict(float)
+        if initial_weights is not None:
+            self.weights.update(initial_weights)
+        self.fcounts = defaultdict(int)
+        self.lrate = lrate
+        self.hlwt = hlwt
+        self.l2wt = l2wt
+        self.sigma = sigma
+        self.train = train
+        self.test = test
+
+    def predict(self, inst, base=2.):
+        if self.method == 'hlr':
+            h = self.halflife(inst, base)
+            p = 2. ** (-inst.t/h)
+            return pclip(p), h
+        elif self.method == 'leitner':
+            try:
+                h = hclip(2. ** inst.fv[0][1])
+            except OverflowError:
+                h = MAX_HALF_LIFE
+            p = 2. ** (-inst.t/h)
+            return pclip(p), h
+        elif self.method == 'pimsleur':
+            try:
+                h = hclip(2. ** (2.35*inst.fv[0][1] - 16.46))
+            except OverflowError:
+                h = MAX_HALF_LIFE
+            p = 2. ** (-inst.t/h)
+            return pclip(p), h
+        elif self.method == 'lr':
+            dp = sum([self.weights[k]*x_k for (k, x_k) in inst.fv])
+            p = 1./(1+math.exp(-dp))
+            return pclip(p), random.random()
+        else:
+            raise Exception
+
+    def train_update(self, inst):
+        if self.method == 'hlr':
+            base = 2.
+            p, h = self.predict(inst, base)
+            dlp_dw = 2.*(p-inst.p)*(LN2**2)*p*(inst.t/h)
+            dlh_dw = 2.*(h-inst.h)*LN2*h
+            for (k, x_k) in inst.fv:
+                rate = (1./(1+inst.p)) * self.lrate / math.sqrt(1 + self.fcounts[k])
+                # rate = self.lrate / math.sqrt(1 + self.fcounts[k])
+                # sl(p) update
+                self.weights[k] -= rate * dlp_dw * x_k
+                # sl(h) update
+                if not self.omit_h_term:
+                    self.weights[k] -= rate * self.hlwt * dlh_dw * x_k
+                # L2 regularization update
+                self.weights[k] -= rate * self.l2wt * self.weights[k] / self.sigma**2
+                # increment feature count for learning rate
+                self.fcounts[k] += 1
+        elif self.method == 'leitner' or self.method == 'pimsleur':
+            pass
+        elif self.method == 'lr':
+            p, _ = self.predict(inst)
+            err = p - inst.p
+            for (k, x_k) in inst.fv:
+                # rate = (1./(1+inst.p)) * self.lrate   / math.sqrt(1 + self.fcounts[k])
+                rate = self.lrate / math.sqrt(1 + self.fcounts[k])
+                # error update
+                self.weights[k] -= rate * err * x_k
+                # L2 regularization update
+                self.weights[k] -= rate * self.l2wt * self.weights[k] / self.sigma**2
+                # increment feature count for learning rate
+                self.fcounts[k] += 1
+
+    def train(self, trainset):
+        if self.method == 'leitner' or self.method == 'pimsleur':
+            return
+        random.shuffle(trainset)
+        for inst in trainset:
+            self.train_update(inst)
+
+    def losses(self, inst):
+        p, h = self.predict(inst)
+        slp = (inst.p - p)**2
+        slh = (inst.h - h)**2
+        return slp, slh, p, h
+
+    def eval(self, testset, prefix=''):
+        results = {'p': [], 'h': [], 'pp': [], 'hh': [], 'slp': [], 'slh': []}
+        for inst in testset:
+            slp, slh, p, h = self.losses(inst)
+            results['p'].append(inst.p)     # ground truth
+            results['h'].append(inst.h)
+            results['pp'].append(p)         # predictions
+            results['hh'].append(h)
+            results['slp'].append(slp)      # loss function values
+            results['slh'].append(slh)
+        mae_p = mae(results['p'], results['pp'])
+        mae_h = mae(results['h'], results['hh'])
+        cor_p = spearmanr(results['p'], results['pp'])
+        cor_h = spearmanr(results['h'], results['hh'])
+        total_slp = sum(results['slp'])
+        total_slh = sum(results['slh'])
+        total_l2 = sum([x**2 for x in self.weights.values()])
+        total_loss = total_slp + self.hlwt*total_slh + self.l2wt*total_l2
+        if prefix:
+            sys.stderr.write('%s\t' % prefix)
+        sys.stderr.write('%.1f (p=%.1f, h=%.1f, l2=%.1f)\tmae(p)=%.3f\tcor(p)=%.3f\tmae(h)=%.3f\tcor(h)=%.3f\n' % \
+            (total_loss, total_slp, self.hlwt*total_slh, self.l2wt*total_l2, \
+            mae_p, cor_p, mae_h, cor_h))
+
+    def dump_weights(self, fname):
+        with open(fname, 'w') as f:
+            for (k, v) in self.weights.items():
+                f.write('%s\t%.4f\n' % (k, v))
+
+    def dump_predictions(self, fname, testset):
+        with open(fname, 'w') as f:
+            f.write('p\tpp\th\thh\tlang\tuser_id\ttimestamp\n')
+            for inst in testset:
+                pp, hh = self.predict(inst)
+                f.write('%.4f\t%.4f\t%.4f\t%.4f\t%s\t%s\t%d\n' % (inst.p, pp, inst.h, hh, inst.lang, inst.uid, inst.ts))
+
+    def dump_detailed_predictions(self, fname, testset):
+        with open(fname, 'w') as f:
+            f.write('p\tpp\th\thh\tlang\tuser_id\ttimestamp\tlexeme_tag\n')
+            for inst in testset:
+                pp, hh = self.predict(inst)
+                for i in range(inst.right):
+                    f.write('1.0\t%.4f\t%.4f\t%.4f\t%s\t%s\t%d\t%s\n' % (pp, inst.h, hh, inst.lang, inst.uid, inst.ts, inst.lexeme))
+                for i in range(inst.wrong):
+                    f.write('0.0\t%.4f\t%.4f\t%.4f\t%s\t%s\t%d\t%s\n' % (pp, inst.h, hh, inst.lang, inst.uid, inst.ts, inst.lexeme))
